2590a49f7f2e1fd54292fcf8a7c218fdb663edaf
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-08-05 00:10:24 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,21 @@
 import express, { Application } from 'express';
 import dotenv from 'dotenv';
-import cors from "cors"
+import cors from "cors";
 import userRouter from './router/user.Router';
-import adminRouter from './router/admin.Router'
+import adminRouter from './router/admin.Router';
+
 dotenv.config();
 
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.use("/api/v1/user",userRouter)
-app.use("/api/v1/admin",adminRouter)
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
 
+const PORT = process.env.PORT || 3000;
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
