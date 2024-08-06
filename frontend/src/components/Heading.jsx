b6543d4d386bc6d581c950c9cbae4c9e7c287630
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-08-06 07:56:49 +0530

    Create Heading.jsx

---
+++ frontend/src/components/Heading.jsx
@@ -0,0 +1,12 @@
+export function Subheading({label}){
+    return <div className="text-2xl py-4 xl:text-4xl font-bold text-blue-900">
+            {label}
+    </div>
+}
+
+export function Heading({label}){
+    return <div className="text-[13px] font-bold text-gray-500">
+        {label}
+    </div>
+}
+
