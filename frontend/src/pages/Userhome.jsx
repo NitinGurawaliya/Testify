1b579cfef54920586ff2ac56c63cdf0a5a04c30b
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-08-06 21:18:45 +0530

    Update Userhome.jsx

--- frontend/src/pages/Userhome.jsx
+++ frontend/src/pages/Userhome.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import UserTestCard from '../components/UserTestCard';
 import UserDashnav from "../components/UserDashnav"
+import { Heading } from '../components/Heading';
 
 const UserDashboard = () => {
   return (
-    <div className="flex flex-col min-h-screen pt-16"> {/* Add padding top to offset fixed navbar */}
+    <div className="flex flex-col min-h-screen pt-16"> {/* Add padding top to offset 
+    fixed navbar */}
       <UserDashnav />
       <div className="flex-1 p-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
