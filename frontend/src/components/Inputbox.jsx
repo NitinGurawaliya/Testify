1c64b6418c38e9b3541ed6c658753b0456aea854
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-08-06 21:19:02 +0530

    Update Inputbox.jsx

--- frontend/src/components/Inputbox.jsx
+++ frontend/src/components/Inputbox.jsx
@@ -3,3 +3,9 @@ export function InputBox({onChange,placeholder}){
      <input className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white" onChange={onChange} placeholder={placeholder} />
     </div>
 }
+
+export function PasswordInputBox({onChange,placeholder}){
+    return <div>
+        <input type="password" className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white" onChange={onChange} placeholder={placeholder} />
+    </div>
+}
