4284c411adf55939ab71933446ad51b7b3001d74
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-08-07 22:32:58 +0530

    Update AdminHome.jsx

--- frontend/src/pages/AdminHome.jsx
+++ frontend/src/pages/AdminHome.jsx
@@ -7,7 +7,11 @@ const AdminDashboard = () => {
     <div className="flex flex-col min-h-screen pt-16"> {/* Add padding top to offset fixed navbar */}
       <AdminDashnav />
       <div className="flex-1 p-4">
+      <div className='flex font-bold text-3xl justify-center my-10'>
+        Created Courses
+      </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+       
           <AdminTestCard />
         </div>
       </div>
