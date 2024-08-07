8a9828a5bef9f2034f73c38d8eef059b64166624
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-08-07 22:32:55 +0530

    Update Userhome.jsx

--- frontend/src/pages/Userhome.jsx
+++ frontend/src/pages/Userhome.jsx
@@ -8,6 +8,9 @@ const UserDashboard = () => {
     <div className="flex flex-col min-h-screen pt-16"> {/* Add padding top to offset 
     fixed navbar */}
       <UserDashnav />
+      <div className='flex font-bold text-3xl justify-center my-10'>
+        User Courses
+      </div>
       <div className="flex-1 p-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
           <UserTestCard />
